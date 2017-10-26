c95dd0743fb01089429de2ea339a6cf7918ccc08
Jordan Klaers <jordan.klaers@gmail.com>
2017-10-26 17:57:08 -0600

    setting up digital leds on pi

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var Gpio = require('pigpio').Gpio
+var Raspi = require("raspi-io");
+var five = require("johnny-five");
+
+var dataPin = new Gpio(17, {mode: Gpio.OUTPUT})
+
+const board = new five.Board({
+        io: new raspi(),
+});
+
+
+board.on("ready", function() {
+  strip = new pixel.Strip({
+    data: dataPin,
+    length: 144, // number of pixels in the strip.
+    board: this,
+    controller: "FIRMATA"
+  });
+
+
+  strip.on("ready", function() {
+        console.log("Strip ready, let's go");
+    });
+
+});
